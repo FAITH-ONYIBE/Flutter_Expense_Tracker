c488e6e4aa772518f2c965bf1a518680839e269d
ONYIBEFAITHEKUMA <ONYIBEFAITHEKUMA@gmail.com>
2025-08-28 17:51:06 +0100

    add gradient FAB and styled bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,55 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
-      body: Text('Home screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[200],
+        // title: Text("Welcome, ${widget.user['username']}"),
+      ),
+      floatingActionButton: Container(
+       decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: LinearGradient(
+           colors: [
+              Color(0xFF00B2E7),
+              Color(0xFFE064F7),
+              Color(0xFFFF8D6C),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+        
+        )
+       ),
+       child: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        shape: CircleBorder(),
+        child: Icon(Icons.add,
+        color: Colors.white,),
+       ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(), 
+        color: Colors.grey[200],
+        notchMargin: 6,
+        child: Row(
+
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            
+            IconButton(onPressed: () {}, icon: Icon(Icons.analytics, ),),
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings, ))
+          ],
+        ),
+      ),
     );
+    
+    
   }
 }
